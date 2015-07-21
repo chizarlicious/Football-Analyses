c65a2e4c3585f86d59e761371eb6bceda1056f04
Charles McEachern <charles.public.account@gmail.com>
2015-07-21 09:59:04 -0500

    looking at how many games are determined by touchdowns

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ import os # for navigating directories
 
 
 
+# note: team names are not always 3 characters. use split(). 
 
 
 
@@ -18,33 +19,38 @@ import os # for navigating directories
 def main():
   # navigate to the root of the data directory
   os.chdir('../data/reco/')
-  
+
+  ngames = 0
+  winnermoretd = 0
+  losermoretd = 0
+  samenumbertd = 0
+
   # scroll through seasons
   seasons = sorted(os.listdir('.'))
   for s in seasons:
   
-    # debug: only look at the first season
-    if seasons.index(s) > 0:
-      break
+#    # debug: only look at the first season
+#    if seasons.index(s) > 0:
+#      break
 
     os.chdir(s)
     games = sorted(os.listdir('.'))
     print s+' season ('+str(len(games))+' games) '
     # scroll through games
     for g in games:
-      print '\tgame '+str(games.index(g))+': '+g[14:24].replace('_',' ')
+    
+#      # debug: only look at the first game
+#      if games.index(g) > 10:
+#        break
 
-      # debug: only look at the first game
-      if games.index(g) > 20:
-        break
-
+#      print '\tgame '+str(games.index(g))+': '+g[14:24].replace('_',' ')
       # load game data from json file into a big dictionary object
       with open(g) as gamefile:
         gamedict = json.load(gamefile)
       # establish data structure to store score data
       teams = ['home', 'away']
-      types = ['touchdown', 'field goal', 'extra point', 'safety']
-      values = {'touchdown':6, 'field goal':3, 'extra point':1, 'safety':2}
+      values = {'touchdown':6, 'field goal':3, 'extra point':1, 'safety':2, 'two point conversion':2}
+      types = values.keys()
       scores = {}
       for tm in teams:
         scores[tm] = {}
@@ -52,20 +58,13 @@ def main():
           scores[tm][ty] = 0
       # scroll through plays to collect scoring data
       for playwrapper in gamedict['plays']:
-
-        # debug: only scroll through the first 10 plays of a game
+#        # debug: only scroll through the first 10 plays of a game
 #        if gamedict['plays'].index(playwrapper) > 50:
 #          break
-
-#        if 'scoring' in playwrapper['play']:
-#          print '\t\t',playwrapper
-
-
         play = playwrapper['play']
         if 'scoring' in play:
           tm = play['scoring']['team']
           ty = play['scoring']['type']
-#          print '\t\t'+tm+' '+ty
           # check for potential key errors
           if tm not in teams:
             print 'unrecognized team: '+tm
@@ -76,23 +75,39 @@ def main():
           # store the score event
           scores[tm][ty] = scores[tm][ty] + 1
       # present the scoring totals for this game
+      totalscore = {}
       for tm in teams:
-        # add up the total for each team
-        total = 0
-        for ty in types:
-          total = total + scores[tm][ty]*values[ty]
-        print '\t\t'+tm+': '+str(total),
-        # also present the score breakdown
-#        for ty in types:
-#          print '\t\t\t'+str(scores[tm][ty])+'x '+ty
-        if scores[tm]['touchdown']==scores[tm]['extra point']:
-          print '\tsame number of touchdowns and extra points'
-        else:
-          print '\tNOT THE SAME number of touchdowns and extra points'
+        totalscore[tm] = sum( [scores[tm][ty]*values[ty] for ty in types] )
+#      for tm in teams:
+#        print '\t\t'+tm+': '+str(scores[tm]['touchdown'])+' touchdowns, '+str(totalscore[tm])+' points total'
+        
+      # figure out who won
+      winner, loser = ('home', 'away') if totalscore['home']>totalscore['away'] else ('away', 'home')
+      
+#      print '\t\twinner: '+winner
+      
+      ngames = ngames + 1
 
+      # did the winner score more touchdowns than the loser?
+      if scores[winner]['touchdown']>scores[loser]['touchdown']:
+#        print '\t\twinner had more touchdowns'
+        winnermoretd = winnermoretd + 1
+      elif scores[winner]['touchdown']<scores[loser]['touchdown']:
+#        print '\t\tloser had more touchdowns'
+        losermoretd = losermoretd + 1
+      else:
+#        print '\t\twinner and loser same number of touchdowns'
+        samenumbertd = samenumbertd + 1
 
     # back out of season directory
     os.chdir('..')
+
+  print 'total games: '+str(ngames)
+  print 'games where the winner scored more touchdowns: '+str(winnermoretd)
+  print 'games where the loser scored more touchdowns: '+str(losermoretd)
+  print 'games where both teams scored equal touchdowns: '+str(samenumbertd)
+
+
 
 # for importability
 if __name__ == '__main__':
